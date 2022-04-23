4cbee68922ff6a17a7447eb7034610cae57e1403
Rakibul Islam Samrat <samratfk123@gmail.com>
2022-04-24 01:07:30 +0600

    Home page complete

--- ParkyWeb/Controllers/HomeController.cs
+++ ParkyWeb/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ParkyWeb.Models;
+using ParkyWeb.Models.ViewModel;
+using ParkyWeb.Repository.IRepository;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -12,15 +14,28 @@ namespace ParkyWeb.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly INationalParkRepository _nationalParkRepository;
+        private readonly ITrailRepository _trailRepository;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,INationalParkRepository nationalParkRepository,
+            ITrailRepository trailRepository)
         {
             _logger = logger;
+           _nationalParkRepository = nationalParkRepository;
+            _trailRepository = trailRepository;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            IndexVM listofParkAndTrails = new IndexVM()
+            { 
+                NationalParkList=await _nationalParkRepository.GetAllAsync(SD.NationalParkAPIPath),
+                TrailList=await _trailRepository.GetAllAsync(SD.TrailAPIPath),
+            
+            };
+
+
+            return View(listofParkAndTrails);
         }
 
         public IActionResult Privacy()
